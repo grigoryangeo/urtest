add3febce34a27339eb1731b1b23519266125e14
xicmiah <xicmiah@gmail.com>
2009-12-12 23:54:17 +0300

    Сделал регистрацию для тестеров

--- bugtracker/forms.py
+++ bugtracker/forms.py
@@ -1,23 +1,26 @@
 # File encoding: utf-8
+from django.core.exceptions import ObjectDoesNotExist
 from django import forms
 import bugtracker.models as models
+from django.contrib.auth.models import User
 
 
 class BugForm(forms.ModelForm):
     short_description = forms.CharField(label="Краткое описание")
     class Meta:
-	model = models.Bug
-	exclude = ['tester', 'status', 'status_comment', 'project']
+        model = models.Bug
+        exclude = ['tester', 'status', 'status_comment', 'project']
 
 
 class ProjectForm(forms.ModelForm):
     class Meta:
-	model = models.Project
-	exclude = ['customer']
+        model = models.Project
+        exclude = ['customer']
 
 
 class TesterForm(forms.Form):
     email = forms.EmailField()
+
     password = forms.CharField(widget=forms.PasswordInput)
     password_confirm = forms.CharField(widget=forms.PasswordInput)
     last_name = forms.CharField(label="Фамилия")
@@ -29,15 +32,29 @@ class TesterForm(forms.Form):
     testing_types = forms.ModelMultipleChoiceField(queryset=models.TestingType.objects.all())
     browsers = forms.ModelMultipleChoiceField(queryset=models.Browser.objects.all())
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(username=email).count() > 0:
+            raise forms.ValidationError('Пользователь с таким адресом уже существует')
+        return email
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        password = cleaned_data['password']
+        password_confirm = cleaned_data['password_confirm']
+        if password != password_confirm:
+            raise forms.ValidationError('Пароль и подтверждение не совпадают')
+        return cleaned_data
+
 
 class UrCustomerForm(forms.ModelForm):
     class Meta:
-	model = models.UrCustomer
+        model = models.UrCustomer
 
 
 class PhysCustomerForm(forms.ModelForm):
     class Meta:
-	model = models.PhysCustomer
+        model = models.PhysCustomer
 
 
 
--- bugtracker/views.py
+++ bugtracker/views.py
@@ -1,5 +1,6 @@
 # File encoding: utf-8
 
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -54,8 +55,23 @@ def tester_registraion(request):
     if request.method == 'POST':
         form = TesterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/testers/')
+            tester = Tester()
+            user = User()
+            user.username = user.email = form.cleaned_data['email']
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            tester.user = user
+            tester.surname = form.cleaned_data['last_name']
+            tester.first_name = form.cleaned_data['first_name']
+            tester.second_name = form.cleaned_data['second_name']
+            tester.email = form.cleaned_data['email']
+            tester.save()
+            tester.osystems = form.cleaned_data['os']
+            tester.program_languages = form.cleaned_data['program_languages']
+            tester.testing_types = form.cleaned_data['testing_types']
+            tester.browsers = form.cleaned_data['browsers']
+            tester.save()
+            return HttpResponseRedirect('/')
     else:
         form = TesterForm()
     return render_to_response('tester_registraion.html',{'form': form})
