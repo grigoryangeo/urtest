682ce417df39878d669451cacde656022dcb062d
xicmiah <xicmiah@gmail.com>
2010-01-27 15:31:34 +0300

    Подрубил приложения sites и flatpages

--- settings.py
+++ settings.py
@@ -77,10 +77,11 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.sites',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.webdesign',
     'django.contrib.formtools',
+    'django.contrib.flatpages',
     'urtest.bugtracker',
 )
 
