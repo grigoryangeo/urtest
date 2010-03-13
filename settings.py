98fb3e6f9b10b26a2360ceb7706e768ff664f1a6
xicmiah <xicmiah@gmail.com>
2010-03-13 17:47:45 +0300

    Добавил неверные настройки SMTP
    Верные настройки хранятся у меня и будут применены при развертывании.
    Закрывает #78, #87, #125

--- settings.py
+++ settings.py
@@ -17,6 +17,12 @@ DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bogus@example.org'
+EMAIL_HOST_PASSWORD = 'bogus'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
