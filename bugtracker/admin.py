3fae4a6d9eb90be6327dc249c68f7c1572bd8caa
xicmiah <xicmiah@gmail.com>
2010-03-24 17:26:14 +0300

    Переработка админки Bugtracker

--- bugtracker/admin.py
+++ bugtracker/admin.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
-from urtest.bugtracker.models import Bug, Tester, Project, Customer, PhysCustomer, UrCustomer, OSystem, Browser, ProgramLang, TestingType, PayingType, Language
+from urtest.bugtracker.models import Bug, Project
 
+class BugAdmin(admin.ModelAdmin):
+    pass
 
-admin.site.register(Bug)
-admin.site.register(Tester)
-admin.site.register(Project)
-admin.site.register(Customer)
-admin.site.register(UrCustomer)
-admin.site.register(PhysCustomer)
-admin.site.register(OSystem)
-admin.site.register(Browser)
-admin.site.register(ProgramLang)
-admin.site.register(TestingType)
-admin.site.register(PayingType)
-admin.site.register(Language)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
 
+admin.site.register(Bug, BugAdmin)
+admin.site.register(Project, ProjectAdmin)
