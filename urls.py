46e831fbe246e4d0fb48f62f5c4721431ab7cd08
xicmiah <xicmiah@gmail.com>
2010-03-24 16:11:31 +0300

    Переработал корневой urlconf

--- urls.py
+++ urls.py
@@ -6,99 +6,25 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-from django.views.generic.simple import direct_to_template
-from django.views.generic import list_detail
-from bugtracker.models import Bug, Project, Customer, PhysCustomer, UrCustomer, Tester
 
-project_info = {
-	"queryset": Project.objects.all(),
-	"template_name": "project_list.html",
-	"template_object_name": "project",
-}
-
-company_info = {
-	"queryset": Customer.objects.all(),
-	"template_name": "company_list.html",
-        "template_object_name": "customer",
-}
-
-tester_info = {
-	"queryset": Tester.objects.all(),
-	"template_name": "tester_list.html",
-	"template_object_name": "tester",
-}
-
-bug_info = {
-    "queryset": Bug.objects.all(),
-    "template_name": "bug_list.html",
-    "template_object_name": "bug",
-}
-
-# Родные для сайта виды
-# Импорт делается автоматически
 urlpatterns = patterns('bugtracker.views',
     # Главная страница
-    #(r'^$', direct_to_template, {'template': 'base.html'}),
+    (r'^$', direct_to_template, {'template': 'main.html'}),
 
-    # Страницы для тестеров:
-    (r'^testers/$', list_detail.object_list, tester_info),
-    # Личная страница тестера
-    (r'^testers/(\d+)$', 'tester_detail'),
-    (r'^testers/(\d+)/projects$', 'tester_detail_projects'),
-    # Регистрация нового тестера
-    (r'^testers/register$', 'tester_registraion'),
-
-    # Страницы компаний:
-    # Список всех компаний
-    (r'^companies/$', list_detail.object_list, company_info),
-    # Личная страница компании
-    (r'^companies/(\d+)(/[a-z]+)?$', 'company_detail'),
-    # Регистрация новой компании
-    (r'^companies/register_f$', 'company_registraion', {'type': 'f'}),
-    (r'^companies/register_y$', 'company_registraion', {'type': 'y'}),
+    # Учетные записи, регистрация и тд
+    (r'^accounts/', include('urtest.accounts.urls')),
     
-    # Общий переход в личный кабинет
-    (r'^my/account', 'redirect_to_self'),
-    
-    (r'^projects/(\d+)/addbug$', 'add_bug'),
-    (r'^thanks$', 'thanks'),
-    # Страницы проектов:
-    # Список всех проектов
-    (r'^projects/$', list_detail.object_list, project_info),
-    # Страница проекта
-    (r'^projects/(\d+)(/[a-z]+)?$', 'project_detail'),
-    # Добавление проекта
-    (r'^projects/new$', 'new_project'),
-    (r'^dogovor$', 'dogovor'),
-    # Баги
-    (r'^bugs/$', list_detail.object_list, bug_info),
-    
-    (r'^bugs/(\d+)$', list_detail.object_list, bug_info),
-    (r'^projects/(\d+)(/[a-z]+/)(\d+)?$','bug_details'),
-    # Example:
-    # (r'^urtest/', include('urtest.foo.urls')),
+    # Проекты, баги
+    (r'^bugtracker/', include('urtest.bugtracker.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-)
-
-urlpatterns += patterns('',
-    # Uncomment the next line to enable the admin:
+    # Админка
     (r'^admin/', include(admin.site.urls)),
 )
 
-# XXX:
-# Предпросмотр форм
-# Заменяем ProjectForm на другое имя формы
-from bugtracker.forms import *
-urlpatterns += patterns('',
-    (r'^formpreview$', StupidFormPreview(ProjectForm)),
-)
-
+# Вход/выход
 urlpatterns += patterns('django.contrib.auth.views',
     # Авторизация
-    (r'^$', 'login', {'template_name': 'main.html'}),
+    (r'^login$', 'login', {'template_name': 'login.html'}),
     # Выход
     (r'^logout$', 'logout', {'template_name': 'logout.html'}),
 )
