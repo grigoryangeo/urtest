ba4f536f36da664a03af6aacb23d089a45ec89eb
GrigoryanGeo <grigoryangeo@gmail.com>
2010-03-27 00:31:17 +0300

    Исправления по последним коментариям

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+#from django.views.generic.create_update import *
 
 urlpatterns = patterns('bugtracker.views',
     (r'^projects/show/(?P<id>\d+)$', 'project_detail'),
     (r'^projects/show/(?P<id>\d+)/testers$', 'project_detail_testers'),
     (r'^projects/show/(?P<id>\d+)/bugs$', 'project_detail_bugs'),
     (r'^projects/enlist/(?P<id>\d+)$', 'project_add_tester'),
-    (r'^projects/list$', 'project_list'),
+    (r'^projects/list$',direct_to_template, {'template': 'project_list.html'}),
     (r'^projects/add$', 'project_add'),
     (r'^projects/add_bug/(?P<project_id>\d+)$', 'project_add_bug'),
 
-    (r'^bugs/show/(?P<id>\d+)$', 'bug_detail'),
-    (r'^bugs/list$', 'bug_list'),
+    (r'^bugs/show/(?P<id>\d+)$','bug_detail'),
+    (r'^bugs/list$', direct_to_template, {'template': 'bug_list.html'}),
 )
--- bugtracker/views.py
+++ bugtracker/views.py
@@ -7,82 +7,56 @@ def project_detail(request, id):
     """
     Детали проекта, вкладка с информацией
     """
-    try:
-        project = Project.objects.get(pk=id)
-    except Project.DoesNotExist:
-        raise Http404
+    project = get_object_or_404(Project, pk=id)
     testers = project.testers.all()
 
-    if request.user.is_authenticated():
-        for tester in testers :
-            if( tester.user == request.user ):
-                project_tester = tester.user
+    tester_in_project = request.user in testers
 
-    return render_to_response('project_detail.html', locals(),
+    return render_to_request('project_detail.html', locals(),
                               context_instance=RequestContext(request))
 
 def project_detail_testers(request, id):
     """
     Детали проекта, вкладка со списком тестеров
     """
-    try:
-        project = Project.objects.get(pk=id)
-    except Project.DoesNotExist:
-        raise Http404
+    project = get_object_or_404(Project, pk=id)
     testers = project.testers.all()
 
-    if request.user.is_authenticated():
-        for tester in testers :
-            if( tester.user == request.user ):
-                project_tester = tester.user
+    tester_in_project = request.user in testers
 
-    return render_to_response('project_testers.html', locals(),
+    return render_to_request('project_testers.html', locals(),
             context_instance=RequestContext(request))
 
 def project_detail_bugs(request, id):
     """
     Детали проекта, вкладка со списком багов
     """
-    try:
-        project = Project.objects.get(pk=id)
-    except Project.DoesNotExist:
-        raise Http404
+    project = get_object_or_404(Project, pk=id)
     testers = project.testers.all()
     bugs = project.bugs.all()
 
-    if request.user.is_authenticated():
-        for tester in testers :
-            if( tester.user == request.user ):
-                project_tester = tester.user
+    tester_in_project = request.user in testers
 
-    return render_to_response('project_bugs.html', locals(),
+    return render_to_request('project_bugs.html', locals(),
             context_instance=RequestContext(request))
 
+@login_required
 def project_add_tester(request, id):
     """
     Добавление авторизованного тестера к проекту
     """
-    try:
-        project = Project.objects.get(pk=id)
-    except Project.DoesNotExist:
-        raise Http404
+    project = get_object_or_404(Project, pk=id)
+
+    if not request.user.is_authenticated():
+        raise PermissionDenied
+
+    if not request.user == tester
+        raise PermissionDenied
     
-    user = request.user
-    if not user.is_authenticated():
-        raise PermissionDenied
-    # Текущий залогиненый пользователь должен быть тестером
-    if not hasattr(user, 'tester'):
-        raise PermissionDenied
-    tester = user.tester
-    project.add_tester(tester)
+    project.add_tester(rquest.user)
     return HttpResponseRedirect('projects/show/%i/testers' % id)
 
-def project_list(request):
-    """
-    Список всех проектов
-    """
-    return render_to_response('project_list.html')
-
+@login_required
 def project_add(request):
     """
     Добавление нового проекта
@@ -94,7 +68,7 @@ def project_add(request):
             return HttpResponseRedirect('/projects/')
     else:
         form = ProjectForm()
-    return render_to_response('new_project.html',{'form': form},
+    return render_to_request('new_project.html',{'form': form},
         context_instance=RequestContext(request))
 
 def project_add_bug(request, project_id):
@@ -108,37 +82,24 @@ def project_add_bug(request, project_id):
             form.save()
             return HttpResponseRedirect('/projects/%s' % project_id)
     else:
-        form = BugForm(initial={'severity':models.Bug.SEVERITY_CHOICES[0][0]} )
-    return render_to_response('addbug.html',locals(),
+        form = BugForm()
+    return render_to_request('addbug.html',locals(),
                               context_instance=RequestContext(request))
 
 def bug_detail(request, id):
     """
     Детали бага
     """
-    try:
-        bugs = Bug.objects.get(pk=id)
-    except Bug.DoesNotExist:
-        raise Http404
+    
+    bug = get_object_or_404(Bug, pk= id)
+    customer_own = request.user == bug.project.customer
 
-    if request.user.is_authenticated():
-        if ( bugs.project.customer.user == request.user)
-            customer_own = project.customer.user
-
-    severity = bugs.get_severity_display()
-    status = bugs.get_status_display()
     if request.method == 'POST':
-        form = BugDetail(request.POST, initial={'status':bugs.status, 'status_comment':bugs.status_comment}, instance=bugs)
+        form = BugDetail(request.POST, instance=bug)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('bugs/show/%s' %id)
     else:
         form = BugDetail(initial={'status':bugs.status,'status_comment':bugs.status_comment})
-    return render_to_response('bug_detail.html',locals(),
+    return render_to_request('bug_detail.html',locals(),
                 context_instance=RequestContext(request))
-
-def bug_list(request):
-    """
-    Список всех багов
-    """
-    return render_to_response('bug_list.html')
