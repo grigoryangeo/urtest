18872ecdd8a2f6229b476c9db5838064f611dd89
xicmiah <xicmiah@gmail.com>
2010-03-28 04:54:39 +0400

    Добавил бекенд с возвращением детального профиля

---
+++ accounts/backends.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.backends import ModelBackend
+from urtest.accounts.models import UserProxy
+
+class UrtestUserModelBackend(ModelBackend):
+    """Бекенд для аутенфикации по моделям urtest
+    
+    Возвращает детальный профиль пользователя
+    """
+    def authenticate(self, username=None, password=None):
+	try:
+	    user = UserProxy.objects.get(username=username)
+	    if user.check_password(password):
+		return user.get_detail()
+	except User.DoesNotExist:
+	    return None
+
+    def get_user(self, user_id):
+	try:
+	    return UserProxy.objects.get(pk=user_id).get_detail()
+	except UserProxy.DoesNotExist:
+	    return None
+
