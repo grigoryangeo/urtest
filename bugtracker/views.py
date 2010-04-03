b90d764d178f002830dd6de2eea8c3b75126a059
xicmiah <xicmiah@gmail.com>
2010-04-03 18:00:23 +0400

    Правка за Гошей: опечатки/приведение к стандарту
    Код приведен к стандарту, но при интеграции будет весело.
    Подчасть #27

--- bugtracker/views.py
+++ bugtracker/views.py
@@ -26,18 +26,26 @@ def project_detail(request, id):
 
     tester_in_project = request.user in testers
 
-    return render_to_request(reguest,'project_detail.html',{'tester_in_project':tester_in_project,'project':project} )
+    return render_to_request(reguest, 'project_detail.html',
+                             {'tester_in_project': tester_in_project,
+                              'project': project,
+                              'testers': testers})
+
 
 def project_detail_testers(request, id):
     """
-    Детали проекта, вкладка со списком тестеров
+    Детали проекта, список тестеров
     """
     project = get_object_or_404(Project, pk=id)
     testers = project.testers.all()
 
     tester_in_project = request.user in testers
 
-    return render_to_request(request,'project_testers.html', {'testers':testers,'tester_in_project':tester_in_project,'project':project} )
+    return render_to_request(request, 'project_testers.html',
+                             {'testers': testers,
+                              'tester_in_project': tester_in_project,
+                              'project': project})
+
 
 def project_detail_bugs(request, id):
     """
@@ -49,7 +57,11 @@ def project_detail_bugs(request, id):
 
     tester_in_project = request.user in testers
 
-    return render_to_request(request,'project_bugs.html', {'bugs':bugs,'tester_in_project':tester_in_project,'project':project} )
+    return render_to_request(request, 'project_bugs.html',
+                             {'bugs': bugs,
+                              'tester_in_project': tester_in_project,
+                              'project': project})
+
 
 @login_required
 def project_add_tester(request, id):
@@ -61,12 +73,13 @@ def project_add_tester(request, id):
     if not request.user.is_authenticated():
         raise PermissionDenied
 
-    if not request.user == tester
+    if not request.user == tester:
         raise PermissionDenied
-    
-    project.add_tester(rquest.user)
-    return HttpResponseRedirect( reverse('project_detail_testers',args=id))
-    #return HttpResponseRedirect(project.get_absolute_url()+'/testers')
+
+    project.add_tester(request.user)
+    return HttpResponseRedirect(reverse('project_detail_testers',
+                                        (), {'id': id}))
+
 
 @login_required
 def project_add(request):
@@ -76,11 +89,12 @@ def project_add(request):
     if request.method == 'POST':
         form = ProjectForm(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/projects/list')
+            project = form.save()
+            return HttpResponseRedirect(project.get_absolute_url())
     else:
         form = ProjectForm()
-    return render_to_request(request,'new_project.html',{'form': form})
+    return render_to_request(request, 'new_project.html', {'form': form})
+
 
 def project_add_bug(request, project_id):
     """
@@ -94,14 +108,17 @@ def project_add_bug(request, project_id):
             return HttpResponseRedirect(project.get_absolute_url())
     else:
         form = BugForm()
-    return render_to_request(request,'addbug.html',{'form':form,'project_id':project_id})
+    return render_to_request(request, 'addbug.html',
+            {'form': form, 'project_id': project_id})
 
+
+@login_required
 def bug_detail(request, id):
     """
     Детали бага
     """
-    
-    bug = get_object_or_404(Bug, pk= id)
+
+    bug = get_object_or_404(Bug, pk=id)
     customer_own = request.user == bug.project.customer
 
     if request.method == 'POST':
@@ -110,5 +127,5 @@ def bug_detail(request, id):
             form.save()
             return HttpResponseRedirect(bug.get_absolute_url())
     else:
-        form = BugDetail(initial={'status':bugs.status,'status_comment':bugs.status_comment})
-    return render_to_request(request,'bug_detail.html',locals())
+        form = BugDetail(instance=bug)
+    return render_to_request(request, 'bug_detail.html', locals())
