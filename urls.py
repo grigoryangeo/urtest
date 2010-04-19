c40d423697de7ee31a9f8f84f5800694f2646baa
xicmiah <xicmiah@gmail.com>
2010-04-19 17:16:45 +0400

    Тестовые ссылки для простотра 404/500

--- urls.py
+++ urls.py
@@ -11,6 +11,8 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('bugtracker.views',
     # Главная страница
     (r'^$', direct_to_template, {'template': 'main.html'}),
+    (r'^404$', direct_to_template, {'template': '404.html'}),
+    (r'^500$', direct_to_template, {'template': '500.html'}),
 
     # Учетные записи, регистрация и тд
     (r'^accounts/', include('urtest.accounts.urls')),
